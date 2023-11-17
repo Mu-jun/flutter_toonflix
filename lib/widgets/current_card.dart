8c505b0387666c47d1de5cb98995af7f0e4f429b
Mu-jun <suo24691@gmail.com>
2023-11-17 18:42:57 +0900

    카드 모양 상세 구현

--- lib/widgets/current_card.dart
+++ lib/widgets/current_card.dart
@@ -25,67 +25,78 @@ class CurrentCard extends StatelessWidget {
       offset: Offset(0, index * offsetY),
       child: Container(
         clipBehavior: Clip.hardEdge,
-        decoration: ShapeDecoration(
-          color: isIverted ? Colors.white : blackColor,
-          shape: const BeveledRectangleBorder(
+        decoration: const ShapeDecoration(
+          shape: BeveledRectangleBorder(
             borderRadius: BorderRadius.only(
               topRight: Radius.circular(50),
               bottomLeft: Radius.circular(50),
             ),
           ),
         ),
-        child: Padding(
-          padding: const EdgeInsets.fromLTRB(20, 25, 0, 50),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    name,
-                    style: TextStyle(
-                        color: isIverted ? blackColor : Colors.white,
-                        fontSize: 30,
-                        fontWeight: FontWeight.w500),
-                  ),
-                  const SizedBox(height: 20),
-                  Row(
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: [
-                      Text(
-                        amount,
-                        style: TextStyle(
+        child: Container(
+          clipBehavior: Clip.hardEdge,
+          decoration: BoxDecoration(
+            color: isIverted ? Colors.white : blackColor,
+            borderRadius: const BorderRadius.only(
+              topLeft: Radius.circular(20),
+              bottomRight: Radius.circular(20),
+              topRight: Radius.circular(75),
+              bottomLeft: Radius.circular(75),
+            ),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(20, 25, 0, 50),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      name,
+                      style: TextStyle(
                           color: isIverted ? blackColor : Colors.white,
-                          fontSize: 20,
+                          fontSize: 30,
+                          fontWeight: FontWeight.w500),
+                    ),
+                    const SizedBox(height: 15),
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        Text(
+                          amount,
+                          style: TextStyle(
+                            color: isIverted ? blackColor : Colors.white,
+                            fontSize: 20,
+                          ),
                         ),
-                      ),
-                      const SizedBox(width: 10),
-                      Text(
-                        code,
-                        style: TextStyle(
-                          color: isIverted
-                              ? blackColor
-                              : Colors.white.withOpacity(0.7),
-                          fontSize: 16,
+                        const SizedBox(width: 10),
+                        Text(
+                          code,
+                          style: TextStyle(
+                            color: isIverted
+                                ? blackColor
+                                : Colors.white.withOpacity(0.7),
+                            fontSize: 16,
+                          ),
                         ),
-                      ),
-                    ],
-                  )
-                ],
-              ),
-              Transform.scale(
-                scale: 2,
-                child: Transform.translate(
-                  offset: const Offset(-20, 20),
-                  child: Icon(
-                    icon,
-                    size: 80,
-                    color: isIverted ? blackColor : Colors.white,
-                  ),
+                      ],
+                    )
+                  ],
                 ),
-              )
-            ],
+                Transform.scale(
+                  scale: 2,
+                  child: Transform.translate(
+                    offset: const Offset(-20, 20),
+                    child: Icon(
+                      icon,
+                      size: 80,
+                      color: isIverted ? blackColor : Colors.white,
+                    ),
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
