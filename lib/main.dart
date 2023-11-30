b154dbae87a4610ed4f943d7e84469a080b13060
Mu-jun <suo24691@gmail.com>
2023-11-30 21:48:40 +0900

    state & setState

--- lib/main.dart
+++ lib/main.dart
@@ -6,131 +6,46 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int count = 0;
+
+  void clickCounter() {
+    setState(() {
+      count += 1;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: const Color(0xFF181818),
-        body: SingleChildScrollView(
-          controller: ScrollController(),
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 10),
-            child: Column(
-              children: [
-                const SizedBox(height: 30),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.end,
-                      children: [
-                        const Text(
-                          'Hey, Selena',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 28,
-                            fontWeight: FontWeight.w500,
-                          ),
-                        ),
-                        Text(
-                          'Welcome back',
-                          style: TextStyle(
-                            color: Colors.white.withOpacity(0.7),
-                            fontSize: 18,
-                          ),
-                        ),
-                      ],
-                    )
-                  ],
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'Click counter',
+                style: TextStyle(fontSize: 40),
+              ),
+              Text(
+                '$count',
+                style: const TextStyle(fontSize: 40),
+              ),
+              IconButton(
+                onPressed: clickCounter,
+                icon: const Icon(
+                  Icons.add_circle_outline_sharp,
+                  size: 40,
                 ),
-                const SizedBox(height: 50),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Total Balance',
-                      style: TextStyle(
-                        color: Colors.white.withOpacity(0.7),
-                        fontSize: 22,
-                      ),
-                    ),
-                    const SizedBox(height: 10),
-                    const Text(
-                      '\$5 194 382',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 42,
-                        fontWeight: FontWeight.w800,
-                      ),
-                    ),
-                    const SizedBox(height: 20),
-                    const Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Button(
-                          text: 'Transfer',
-                          textColor: Colors.black,
-                          backgroundColor: Color(0xFFF2B33A),
-                        ),
-                        Button(
-                          text: 'Request',
-                          textColor: Colors.white,
-                          backgroundColor: Color(0xFF1F2123),
-                        ),
-                      ],
-                    )
-                  ],
-                ),
-                const SizedBox(height: 75),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: [
-                    const Text(
-                      'Wallets',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 35,
-                        fontWeight: FontWeight.w500,
-                      ),
-                    ),
-                    Text(
-                      'View All',
-                      style: TextStyle(
-                        color: Colors.white.withOpacity(0.7),
-                        fontSize: 18,
-                      ),
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 20),
-                const CurrentCard(
-                  name: 'Euro',
-                  code: 'EUR',
-                  amount: '6 428',
-                  icon: Icons.euro_symbol,
-                  index: 0,
-                ),
-                const CurrentCard(
-                  name: 'Dollar',
-                  code: 'USD',
-                  amount: '55 622',
-                  icon: Icons.monetization_on_outlined,
-                  isIverted: true,
-                  index: 1,
-                ),
-                const CurrentCard(
-                  name: 'Rupee',
-                  code: 'INR',
-                  amount: '28 981',
-                  icon: Icons.currency_rupee,
-                  index: 2,
-                ),
-              ],
-            ),
+              )
+            ],
           ),
         ),
       ),
